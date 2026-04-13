b258c90111a98a500d116911e28ed664d02a3836
mirulav-art <mirulav.art@gmail.com>
2026-04-13 22:24:36 +0300

    npccc

---
+++ npc_16396.lua
@@ -0,0 +1,10 @@
+-- NPC Entry: 16396
+-- Event: OnEnterCombat -> Yell
+
+local NPC_ENTRY = 16396
+
+local function OnEnterCombat(event, creature, target)
+    creature:SendUnitYell("Aha, stai ca pun mana pe tine !", 0)
+end
+
+RegisterCreatureEvent(NPC_ENTRY, 1, OnEnterCombat)
