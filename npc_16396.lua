7feb535e5fad6cc072ebfbc3f4585adff064328f
mirulav-art <mirulav.art@gmail.com>
2026-04-13 23:22:02 +0300

    asdsd

--- npc_16396.lua
+++ npc_16396.lua
@@ -1,14 +1,26 @@
 -- NPC Entry: 16396
--- Event: OnEnterCombat -> Yell
 
 local NPC_ENTRY = 16396
+local spellTimer = 0
 
 local function OnEnterCombat(event, creature, target)
     creature:SendUnitYell("Aha, stai ca pun mana pe tine " .. target:GetName() .. "!", 0)
     creature:CastSpell(target, 71961, true)
-
+    spellTimer = 10000
 end
 
+local function OnAIUpdate(event, creature, diff)
+    if not creature:IsInCombat() then return end
 
+    spellTimer = spellTimer - diff
+    if spellTimer <= 0 then
+        local target = creature:GetVictim()
+        if target then
+            creature:CastSpell(target, 37572, true)
+        end
+        spellTimer = 10000
+    end
+end
 
 RegisterCreatureEvent(NPC_ENTRY, 1, OnEnterCombat)
+RegisterCreatureEvent(NPC_ENTRY, 7, OnAIUpdate)
